8e10e7199295ba334c9fb8b8c6a94f18187886be
Chris Stehno <chris.stehno@netscout.com>
2023-05-19 10:53:33 -0500

    added requirements.txt and fixed linter issues

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+"""Module to provide a CLI for the encounter difficulty calculation."""
 import click
 
 from difficulty import calculate_difficulty
@@ -15,8 +16,9 @@ from difficulty import calculate_difficulty
     help='The count and CR values for each monster, as n@CR.'
 )
 def difficulty(party: str, monsters: str):
+    """Command line interface wrapper around the `calculate_difficulty` function."""
     click.echo(f"{calculate_difficulty(party, monsters)}")
 
 
 if __name__ == '__main__':
-    difficulty()
+    difficulty()  # pylint: disable=no-value-for-parameter
